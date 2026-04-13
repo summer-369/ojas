4d0184a79f311c505491065d1d75bc536f673086
summer-369 <ojasmassagespa@gmail.com>
2026-04-13 11:22:37 -0400

    Update opening-time-data.ts

--- ojas/src/data/opening-time-data.ts
+++ ojas/src/data/opening-time-data.ts
@@ -1,19 +1,19 @@
 export const openingTimeData = [
   {
     day_of_week: "Mon",
-    hours: "10:00 AM - 7:00 PM",
+    hours: "10:00 AM - 8:00 PM",
   },
   {
     day_of_week: "Tue",
-    hours: "10:00 AM - 7:00 PM",
+    hours: "10:00 AM - 8:00 PM",
   },
   {
     day_of_week: "Wed",
-    hours: "10:00 AM - 7:00 PM",
+    hours: "10:00 AM - 8:00 PM",
   },
   {
     day_of_week: "Thu",
-    hours: "10:00 AM - 7:00 PM",
+    hours: "10:00 AM - 8:00 PM",
   },
   {
     day_of_week: "Fri",
@@ -25,6 +25,6 @@ export const openingTimeData = [
   },
   {
     day_of_week: "Sun",
-    hours: "10:00 AM - 5:00 PM",
+    hours: "10:00 AM - 6:00 PM",
   },
 ];
